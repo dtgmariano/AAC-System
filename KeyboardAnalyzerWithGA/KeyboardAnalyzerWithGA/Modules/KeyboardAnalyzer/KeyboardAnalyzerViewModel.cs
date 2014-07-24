3fca788fdee0f0f193dcff2e8390cdbdf8b1811b
Daniel Mariano <dtgm@uol.com.br>
2014-07-24 13:11:54 -0300

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	KeyboardAnalyzerWithGA/KeyboardAnalyzerWithGA.suo
    
    	KeyboardAnalyzerWithGA/KeyboardAnalyzerWithGA/bin/Debug/KeyboardAnalyzerWithGA.exe
    
    	KeyboardAnalyzerWithGA/KeyboardAnalyzerWithGA/bin/Debug/KeyboardAnalyzerWithGA.pdb
    
    	KeyboardAnalyzerWithGA/KeyboardAnalyzerWithGA/obj/x86/Debug/KeyboardAnalyzerWithGA.csproj.FileListAbsolute.txt
    
    	KeyboardAnalyzerWithGA/KeyboardAnalyzerWithGA/obj/x86/Debug/KeyboardAnalyzerWithGA.exe
    
    	KeyboardAnalyzerWithGA/KeyboardAnalyzerWithGA/obj/x86/Debug/KeyboardAnalyzerWithGA.pdb

--- KeyboardAnalyzerWithGA/KeyboardAnalyzerWithGA/Modules/KeyboardAnalyzer/KeyboardAnalyzerViewModel.cs
+++ KeyboardAnalyzerWithGA/KeyboardAnalyzerWithGA/Modules/KeyboardAnalyzer/KeyboardAnalyzerViewModel.cs
@@ -14,8 +14,8 @@ namespace KeyboardAnalyzerWithGA
         KeyboardAnalyzerModel ka;
         int criteria_number_suggestion = 4;
         bool hasFrequency = true;
-        //string dictionaryFilePath = "C:\\Users\\Biolab\\GitHub\\AAC-System\\KeyboardAnalyzerWithGA\\KeyboardAnalyzerWithGA\\Data\\dictionary_freq_pt_br.txt";
-        string dictionaryFilePath = "C:\\Users\\Daniel\\GitHub\\AAC-System\\KeyboardAnalyzerWithGA\\KeyboardAnalyzerWithGA\\Data\\dictionary_freq_pt_br.txt";
+        string dictionaryFilePath = "C:\\Users\\Biolab\\GitHub\\AAC-System\\KeyboardAnalyzerWithGA\\KeyboardAnalyzerWithGA\\Data\\dictionary_freq_pt_br.txt";
+        //string dictionaryFilePath = "C:\\Users\\Daniel\\GitHub\\AAC-System\\KeyboardAnalyzerWithGA\\KeyboardAnalyzerWithGA\\Data\\dictionary_freq_pt_br.txt";
         string outputFilePath = "data.txt";
         public KeyboardAnalyzerViewModel()
         {
